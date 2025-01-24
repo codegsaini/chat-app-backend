8fe44d03f16a41784654d36eb9c5222ade7f8d03
Gaurav Saini <codegsaini@gmail.com>
2025-01-24 17:29:44 +0530

    fix: Add chat auto update capability

--- controller/CreateUser.js
+++ controller/CreateUser.js
@@ -1,47 +1,48 @@
-import { text } from "express";
-import prisma from "../prisma/prismaInstance.js";
+import { text } from 'express';
+import prisma from '../prisma/prismaInstance.js';
 
 export const createUser = async (req, res, next) => {
-    try {
-        const { name, email, phone, pageTitle } = req.body;
-        if (!name || !email || !phone)
-            res.status(500).json({
-                error: "empty-field",
-                message: "Field should not be empty"
-            });
+	try {
+		const { name, email, phone, pageTitle } = req.body;
+		if (!name || !email || !phone)
+			res.status(500).json({
+				error: 'empty-field',
+				message: 'Field should not be empty',
+			});
 
-        const user = await prisma.user.create({
-            data: {
-                name: name,
-                email: email,
-                phone: phone
-            }
-        });
-        
-        const ticket = await prisma.ticket.create({
-            data: {
-                userId: user.id,
-                orgId: "org1",
-                page: pageTitle,
-                resolved: false
-            }
-        })
-    
-        res.json({
-            uid: user.id,
-            ticketId: ticket.id
-        });
-    } catch(e) {
-        res.status(500).json({
-            error: "unknown-error",
-            message: e?.message || "Something went wrong"
-        })
-    }
-}
+		const user = await prisma.user.create({
+			data: {
+				name: name,
+				email: email,
+				phone: phone,
+			},
+		});
+
+		const ticket = await prisma.ticket.create({
+			data: {
+				userId: user.id,
+				orgId: 'org1',
+				page: pageTitle,
+				resolved: false,
+			},
+		});
+
+		res.json({
+			uid: user.id,
+			ticketId: ticket.id,
+			orgId: ticket.orgId,
+		});
+	} catch (e) {
+		res.status(500).json({
+			error: 'unknown-error',
+			message: e?.message || 'Something went wrong',
+		});
+	}
+};
 
 const parseTitle = (body) => {
-    let match = body.match(/<title>([^<]*)<\/title>/) // regular expression to parse contents of the <title> tag
-    if (!match || typeof match[1] !== 'string')
-      throw new Error('Unable to parse the title tag')
-    return match[1]
-  }
+	let match = body.match(/<title>([^<]*)<\/title>/); // regular expression to parse contents of the <title> tag
+	if (!match || typeof match[1] !== 'string')
+		throw new Error('Unable to parse the title tag');
+	return match[1];
+};
--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-import express from "express";
-import cors from "cors";
-import { configDotenv } from "dotenv";
-import {createServer} from "node:http";
-import {Server} from "socket.io";
-import userRoutes from "./routes/user-routes.js";
-import chatRoutes from "./routes/chat-routes.js";
-import ticketRoutes from "./routes/ticket-routes.js";
-import prisma from "./prisma/prismaInstance.js";
+import express from 'express';
+import cors from 'cors';
+import { configDotenv } from 'dotenv';
+import { createServer } from 'node:http';
+import { Server } from 'socket.io';
+import userRoutes from './routes/user-routes.js';
+import chatRoutes from './routes/chat-routes.js';
+import ticketRoutes from './routes/ticket-routes.js';
+import prisma from './prisma/prismaInstance.js';
 configDotenv();
-
 
 const PORT = 8080;
 
@@ -22,49 +21,74 @@ app.use(userRoutes);
 app.use(chatRoutes);
 app.use(ticketRoutes);
 io.on('connection', (socket) => {
-    socket.on("connectChat", async (data) => {
-        // const data = JSON.parse(dataObj);
-        const ticketId = data.ticketId;
-        socket.join(ticketId);
-        socket.emit("connected");
-    });
-    socket.on("resolve", async (data) => {
-        /*
-        *   Validation for company here...
-        */
-        const ticketId = data.ticketId;
-        try {
-            await prisma.ticket.update({
-                where: {id: ticketId},
-                data: {
-                    resolved: true
-                }
-            });
-            io.sockets.in(ticketId).emit("resolved");
-        } catch(e) {
-            io.sockets.in(ticketId).emit("error", "Record not found");
-        }
-        
-    });
+	socket.on('connectChat', async (data) => {
+		const ticketId = data.ticketId;
+		const orgId = data.orgId;
+		socket.join(ticketId);
+		if (orgId) socket.join(orgId);
+		socket.emit('connected');
+	});
+	socket.on('joinOrg', async (data) => {
+		const orgId = data.orgId;
+		socket.join(orgId);
+		socket.emit('joinedOrg');
+	});
+	socket.on('resolve', async (data) => {
+		/*
+		 *   Validation for company here...
+		 */
+		const ticketId = data.ticketId;
+		try {
+			await prisma.ticket.update({
+				where: { id: ticketId },
+				data: {
+					resolved: true,
+				},
+			});
+			io.sockets.in(ticketId).emit('resolved');
+		} catch (e) {
+			io.sockets.in(ticketId).emit('error', 'Record not found');
+		}
+	});
 
-    socket.on("chat", async(data) => {
-        // const data = JSON.parse(dataObj);
-        const uid = data.uid;
-        const receiverId = data.receiverId;
-        const ticketId = data.ticketId;
-        const reply = data.reply;
-        const message = await prisma.chat.create({
-            data: {
-                senderId: uid,
-                receiverId: receiverId,
-                ticketId: ticketId,
-                message: reply
-            }
-        });
-        io.sockets.in(ticketId).emit("reply", (({id, ...restObj}) => restObj)(message));
-    });
-})
+	socket.on('informNewChat', async (data) => {
+		try {
+			const ticketId = data.ticketId;
+			const ticket = await prisma.ticket.findUnique({
+				where: { id: ticketId },
+			});
+			let response = { ticket: ticket };
+			const user = await prisma.user.findUnique({
+				where: { id: ticket.userId },
+			});
+			response['user'] = user;
+			response['page'] = ticket.page;
+			response['lastMessage'] = data?.latestChat || '';
+			io.sockets.in(data.orgId).emit('newChat', response);
+		} catch (e) {
+			io.sockets.in(data.ticketId).emit('error', 'Something went wrong');
+		}
+	});
+	socket.on('chat', async (data) => {
+		// const data = JSON.parse(dataObj);
+		const uid = data.uid;
+		const receiverId = data.receiverId;
+		const ticketId = data.ticketId;
+		const reply = data.reply;
+		const message = await prisma.chat.create({
+			data: {
+				senderId: uid,
+				receiverId: receiverId,
+				ticketId: ticketId,
+				message: reply,
+			},
+		});
+		io.sockets
+			.in(ticketId)
+			.emit('reply', (({ id, ...restObj }) => restObj)(message));
+	});
+});
 
 server.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
-})
+	console.log(`Server is listening on port ${PORT}`);
+});
